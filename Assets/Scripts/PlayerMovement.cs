bac8dc4c551d6fe72d10f4c6591ebf8b4b327363
Neo_ <paulo.henriqq8@gmail.com>
2025-10-17 20:58:17 -0400

    Add Player animation
    Idle and running animations.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,25 @@
 using System;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float jumpForce = 10f;
     InputAction moveAction;
+    Vector2 moveValue;
     InputAction jumpAction;
     Rigidbody2D myRigidbody2D;
     BoxCollider2D myFeetCollider2D;
+    Animator myAnimator;
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myFeetCollider2D = GetComponent<BoxCollider2D>();
+        myAnimator = GetComponent<Animator>();
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
     }
@@ -25,17 +29,18 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Jump();
+        Flip();
     }
 
 
     void Move()
     {
-        // if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
-
-        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        moveValue = moveAction.ReadValue<Vector2>();
         myRigidbody2D.linearVelocity = new Vector2(moveValue.x * movementSpeed, myRigidbody2D.linearVelocity.y);
+        bool hasHorizontalSpeed = Mathf.Abs(moveValue.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", hasHorizontalSpeed);
     }
-    
+
     void Jump()
     {
         if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
@@ -43,6 +48,16 @@ public class PlayerMovement : MonoBehaviour
         if (jumpAction.IsPressed())
         {
             myRigidbody2D.linearVelocity = new Vector2(0f, jumpForce);
+        }
+    }
+    
+    void Flip()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(moveValue.x) > Mathf.Epsilon;
+
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(moveValue.x), 1f);
         }
     }
 }
