8990b32cf3284794e7065f7c9bc32be38a94bf92
Neo_ <paulo.henriqq8@gmail.com>
2025-10-17 20:37:25 -0400

    Add Player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float jumpForce = 10f;
     InputAction moveAction;
+    InputAction jumpAction;
     Rigidbody2D myRigidbody2D;
     BoxCollider2D myFeetCollider2D;
     void Start()
@@ -15,20 +17,32 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myFeetCollider2D = GetComponent<BoxCollider2D>();
         moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
 
     void Update()
     {
         Move();
+        Jump();
     }
-    
-     
+
+
     void Move()
     {
-        if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
-        
+        // if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
+
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
         myRigidbody2D.linearVelocity = new Vector2(moveValue.x * movementSpeed, myRigidbody2D.linearVelocity.y);
+    }
+    
+    void Jump()
+    {
+        if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
+
+        if (jumpAction.IsPressed())
+        {
+            myRigidbody2D.linearVelocity = new Vector2(0f, jumpForce);
+        }
     }
 }
