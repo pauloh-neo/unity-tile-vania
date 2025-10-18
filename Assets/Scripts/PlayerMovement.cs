58c5d4a37e761a19ed82c22091f4281c4baeabbe
Neo_ <paulo.henriqq8@gmail.com>
2025-10-17 23:41:15 -0400

    Feature Death fading effect

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.Collections;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,38 +10,55 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float jumpForce = 10f;
+    [SerializeField] bool isAlive;
+    [SerializeField] Vector2 deathKick = new Vector2(10f,10f);
+    [SerializeField] float fadeSpeed = 1f;
+    [SerializeField] float targetAlpha = 0;
     InputAction moveAction;
     Vector2 moveValue;
     InputAction jumpAction;
     Rigidbody2D myRigidbody2D;
     BoxCollider2D myFeetCollider2D;
     CapsuleCollider2D myBodyCollider2D;
+    SpriteRenderer mySpriterenderer;
     Animator myAnimator;
     float playerGravity;
+
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myFeetCollider2D = GetComponent<BoxCollider2D>();
         myBodyCollider2D = GetComponent<CapsuleCollider2D>();
+        mySpriterenderer = GetComponent<SpriteRenderer>();
         myAnimator = GetComponent<Animator>();
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
         playerGravity = myRigidbody2D.gravityScale;
+        isAlive = true;
+        
     }
 
 
     void Update()
     {
+        if (!isAlive)
+        {
+            FadingEffect();
+            return;
+        }
+        
         Move();
         Jump();
         Flip();
         Climb();
+        Die();
         
     }
 
 
     void Move()
     {
+        if (!isAlive) return;
         moveValue = moveAction.ReadValue<Vector2>();
         myRigidbody2D.linearVelocity = new Vector2(moveValue.x * movementSpeed, myRigidbody2D.linearVelocity.y);
         bool hasHorizontalSpeed = Mathf.Abs(moveValue.x) > Mathf.Epsilon;
@@ -74,7 +92,7 @@ public class PlayerMovement : MonoBehaviour
             myRigidbody2D.linearVelocity = new Vector2(0f, jumpForce);
         }
     }
-    
+
     void Flip()
     {
         bool hasHorizontalSpeed = Mathf.Abs(moveValue.x) > Mathf.Epsilon;
@@ -84,4 +102,22 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(Mathf.Sign(moveValue.x), 1f);
         }
     }
+
+    void Die()
+    {
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            myRigidbody2D.linearVelocity = deathKick; 
+        }
+    }
+    
+    void FadingEffect()
+    {
+        Color color = mySpriterenderer.color;
+        color.a = Mathf.MoveTowards(color.a, targetAlpha, fadeSpeed * Time.deltaTime);
+        mySpriterenderer.color = color;
+    }
+    
 }
