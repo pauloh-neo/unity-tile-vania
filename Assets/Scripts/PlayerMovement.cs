7a6dc0db08acc8b1db60b96fff9ed4df540d781a
Neo_ <paulo.henriqq8@gmail.com>
2025-10-17 21:29:05 -0400

    Add Climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,14 +14,18 @@ public class PlayerMovement : MonoBehaviour
     InputAction jumpAction;
     Rigidbody2D myRigidbody2D;
     BoxCollider2D myFeetCollider2D;
+    CapsuleCollider2D myBodyCollider2D;
     Animator myAnimator;
+    float playerGravity;
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myFeetCollider2D = GetComponent<BoxCollider2D>();
+        myBodyCollider2D = GetComponent<CapsuleCollider2D>();
         myAnimator = GetComponent<Animator>();
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        playerGravity = myRigidbody2D.gravityScale;
     }
 
 
@@ -30,6 +34,8 @@ public class PlayerMovement : MonoBehaviour
         Move();
         Jump();
         Flip();
+        Climb();
+        
     }
 
 
@@ -41,9 +47,27 @@ public class PlayerMovement : MonoBehaviour
         myAnimator.SetBool("isRunning", hasHorizontalSpeed);
     }
 
+    void Climb()
+    {
+        if (myBodyCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            myRigidbody2D.linearVelocity = new Vector2(myRigidbody2D.linearVelocity.x, moveValue.y * movementSpeed);
+            bool hasVerticalSpeed = Mathf.Abs(moveValue.y) > Mathf.Epsilon;
+            myRigidbody2D.gravityScale = 0;
+            myAnimator.SetBool("isClimbing", hasVerticalSpeed);
+        }
+        else
+        {
+            myRigidbody2D.gravityScale = playerGravity;
+            myAnimator.SetBool("isClimbing", false);
+        }
+
+    }
+
     void Jump()
     {
         if (!myFeetCollider2D.IsTouchingLayers(LayerMask.GetMask("Platform"))) return;
+        
 
         if (jumpAction.IsPressed())
         {
